f221abdbcea6945cb3f749f9d2a2e3b36e4fb530
Lexie <oleksandrapas@gmail.com>
2019-12-26 17:20:50 +0100

    functionality: adding ingredients from recipe to shopping list is implemented

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,16 +11,24 @@ export class RecipeService {
     new Recipe(
       "A Test Recipe 1",
       "Description 1",
-      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"
+      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
+      [new Ingredient("Meat", 5), new Ingredient("Avocado", 3)]
     ),
     new Recipe(
       "A Test Recipe 2",
       "Description 2",
-      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"
+      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
+      [new Ingredient("Potato", 8), new Ingredient("Carrot", 5)]
     )
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipe() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
