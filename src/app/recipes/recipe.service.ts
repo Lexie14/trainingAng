58a09457d6389b74862856caa77fa0e958e32e97
Alex <alex@MacBook-Pro-Alex.local>
2020-02-07 13:43:02 +0100

    sending and fetching recipes from the Firebase implemented, lesson 261 finished

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,22 +9,29 @@ export class RecipeService {
   recipeSelected = new Subject<Recipe>();
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      "A Test Recipe 1",
-      "Description 1",
-      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
-      [new Ingredient("Meat", 5), new Ingredient("Avocado", 3)]
-    ),
-    new Recipe(
-      "A Test Recipe 2",
-      "Description 2",
-      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
-      [new Ingredient("Potato", 8), new Ingredient("Carrot", 5)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "A Test Recipe 1",
+  //     "Description 1",
+  //     "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
+  //     [new Ingredient("Meat", 5), new Ingredient("Avocado", 3)]
+  //   ),
+  //   new Recipe(
+  //     "A Test Recipe 2",
+  //     "Description 2",
+  //     "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg",
+  //     [new Ingredient("Potato", 8), new Ingredient("Carrot", 5)]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
