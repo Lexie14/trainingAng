ce44fdd4b2ca111f41c5d15bbf333ed3cc7eb233
Alex <alex@MacBook-Pro-Alex.local>
2020-02-19 21:39:21 +0100

    Token attached, logout, autologin and autologout implemented

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -18,8 +19,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-            .get<Recipe[]>('https://ng-course-recipe-book-67e84.firebaseio.com/recipes.json')
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-67e84.firebaseio.com/recipes.json')
             .pipe(
                 map(recipes => {
                     return recipes.map(recipe => {
@@ -28,6 +28,7 @@ export class DataStorageService {
                 }),
                 tap(recipes => {
                     this.recipeService.setRecipes(recipes);
-                }))
+                })
+            )
     }
 }
