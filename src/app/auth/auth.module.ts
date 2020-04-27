8f2f875c748a03112adc42d0394ad928233b593e
Lexie <oleksandrapas@gmail.com>
2020-04-27 22:02:24 +0200

    lazy loading for Auth and Shopping-List Modules adde

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { FormsModule } from "@angular/forms";
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: "auth", component: AuthComponent }]),
+    RouterModule.forChild([{ path: "", component: AuthComponent }]),
     SharedModule
   ]
 })
