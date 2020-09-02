88a6d5b4f73ae68247266da2d50319aabd8f84d8
Lexie <oleksandrapas@gmail.com>
2020-09-02 21:29:11 +0200

    Login Effect for NgRx added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { HeaderComponent } from "./header/header.component";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
 import * as fromApp from "./store/app.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { AuthEffects } from "./auth/store/auth.effects";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,25 @@
-import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  ViewChild,
+  OnDestroy,
+  OnInit
+} from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
 import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AlertComponent } from "../shared/alert/alert.component";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import * as AuthActions from "../auth/store/auth.actions";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html"
 })
-export class AuthComponent implements OnDestroy{
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -21,8 +30,19 @@ export class AuthComponent implements OnDestroy{
   constructor(
     private authService: AuthService,
     private router: Router,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>
   ) {}
+
+  ngOnInit() {
+    this.store.select("auth").subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+      if (this.error) {
+        this.showErrorAlert(this.error);
+      }
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -38,21 +58,25 @@ export class AuthComponent implements OnDestroy{
     this.isLoading = true;
 
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
+      // authObs = this.authService.login(email, password);
+      this.store.dispatch(
+        new AuthActions.LoginStart({ email: email, password: password })
+      );
     } else {
       authObs = this.authService.signup(email, password);
     }
-    authObs.subscribe(
-      resData => {
-        this.isLoading = false;
-        this.router.navigate(["/recipes"]);
-      },
-      errorMessage => {
-        this.error = errorMessage;
-        this.showErrorAlert(errorMessage);
-        this.isLoading = false;
-      }
-    );
+
+    // authObs.subscribe(
+    //   resData => {
+    //     this.isLoading = false;
+    //     this.router.navigate(["/recipes"]);
+    //   },
+    //   errorMessage => {
+    //     this.error = errorMessage;
+    //     this.showErrorAlert(errorMessage);
+    //     this.isLoading = false;
+    //   }
+    // );
     form.reset();
   }
 
@@ -75,8 +99,8 @@ export class AuthComponent implements OnDestroy{
   }
 
   ngOnDestroy() {
-      if (this.closeSub) {
-          this.closeSub.unsubscribe();
-      }
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 }
