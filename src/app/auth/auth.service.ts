540bef2a1914ec69909448fb41bb572deccd0d56
Lexie <oleksandrapas@gmail.com>
2020-09-04 14:51:04 +0200

    code cleaned

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,23 +1,10 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { User } from "./auth.model";
 import { Store } from "@ngrx/store";
 import * as fromApp from "../store/app.reducer";
 import * as AuthActions from "./store/auth.actions";
 
-export interface AuthResponseData {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
-
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
   constructor(private store: Store<fromApp.AppState>) {}
