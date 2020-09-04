f9aa94e89a0118c7b32295e12d82497c42a130e2
Lexie <oleksandrapas@gmail.com>
2020-09-04 17:57:35 +0200

    Fetching recipe detail data via NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { StoreModule } from "@ngrx/store";
-
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -11,6 +12,7 @@ import { CoreModule } from "./core.module";
 import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -18,6 +20,8 @@ import { AuthEffects } from "./auth/store/auth.effects";
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import * as fromApp from "../../store/app.reducer";
+import * as RecipesActions from "../store/recipe.actions";
+import { map, switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-recipe-detail",
@@ -12,27 +17,46 @@ export class RecipeDetailComponent implements OnInit {
   recipeSelected: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
+  constructor(
+    private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.id = +params['id'];
-      this.recipeSelected = this.recipeService.getRecipe(this.id);
-    })
+    this.route.params
+      .pipe(
+        map(params => {
+          return +params["id"];
+        }),
+        switchMap(id => {
+          this.id = id;
+          return this.store.select("recipes");
+        }),
+        map(recipesState => {
+          return recipesState.recipes.find((recipe, index) => {
+            return index === this.id;
+          });
+        })
+      )
+      .subscribe(recipe => {
+        this.recipeSelected = recipe;
+      });
   }
 
   onAddToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipeSelected.ingredients);
+    this.recipeService.addIngredientsToShoppingList(
+      this.recipeSelected.ingredients
+    );
   }
 
   onEditRecipe() {
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(["edit"], { relativeTo: this.route });
   }
 
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
-    this.router.navigate(['/recipes']);
+    this.router.navigate(["/recipes"]);
   }
 }
